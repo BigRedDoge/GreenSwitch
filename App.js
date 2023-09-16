4f0610582d544708e85f296ba086ad9e7cf38fad
Michael Cox <95450502+michaelcox3@users.noreply.github.com>
2023-09-16 17:03:19 -0400

    oveerride

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './app/Login';
+import SurveyScreen from './app/Survey';
+import ResultScreen from './app/Result';
 
 export default function App() {
+
+
+
+  // Define the onPressLearnMore function
+  // const onPressLearnMore = () => {
+  //   Alert.alert('Button Pressed!', 'You pressed the Learn More button.');
+  // };
+
+  // const onPressLogin = () => {
+  //   if(username=="michael1" && password=="123") {
+  //     Alert.alert("Logging in...");
+  //   }
+  // };
+
+  // const [username, onChangeUsername] = React.useState('');
+  // const [password, onChangePassword] = React.useState('');
+
+  const Stack = createStackNavigator();
+
   return (
-    <View style={styles.container}>
-      <Text> up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen}/>
+        <Stack.Screen name="Survey" component={SurveyScreen}/>
+        <Stack.Screen name="Result" component={ResultScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
